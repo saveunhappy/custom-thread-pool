6c34177129b3885f01ea7014b7ac2440165e2362
wg2049 <wg2049@hotmail.com>
2019-11-29 11:14:29 +0800

    完成线程池创建 (#3)
    完成线程池创建 (#3)

--- src/main/java/com/github/hcsp/multithread/MyThreadPools.java
+++ src/main/java/com/github/hcsp/multithread/MyThreadPools.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.multithread;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class MyThreadPools {
     // 创建一个线程池，满足以下要求：
@@ -8,6 +11,43 @@ public class MyThreadPools {
     // 任务等待队列大小为20，如果超过20继续往该线程池中提交任务，这些任务会被悄悄丢弃
     // 线程的名字为"MyThread"
     public static ExecutorService myThreadPool() {
-        return null;
+
+        /*
+        *
+        * public ThreadPoolExecutor(int corePoolSize,
+                              int maximumPoolSize,
+                              long keepAliveTime,
+                              TimeUnit unit,
+                              BlockingQueue<Runnable> workQueue,
+                              ThreadFactory threadFactory,
+                              RejectedExecutionHandler handler) {
+        if (corePoolSize < 0 ||
+            maximumPoolSize <= 0 ||
+            maximumPoolSize < corePoolSize ||
+            keepAliveTime < 0)
+            throw new IllegalArgumentException();
+        if (workQueue == null || threadFactory == null || handler == null)
+            throw new NullPointerException();
+        this.acc = System.getSecurityManager() == null ?
+                null :
+                AccessController.getContext();
+        this.corePoolSize = corePoolSize;
+        this.maximumPoolSize = maximumPoolSize;
+        this.workQueue = workQueue;
+        this.keepAliveTime = unit.toNanos(keepAliveTime);
+        this.threadFactory = threadFactory;
+        this.handler = handler;
+         }
+        * */
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(
+                10,
+                10,
+                60,
+                TimeUnit.MICROSECONDS,
+                new LinkedBlockingQueue<Runnable>(20),
+                (r) -> new Thread(r, "MyThread"),
+                new ThreadPoolExecutor.DiscardPolicy()
+        );
+        return poolExecutor;
     }
 }
