3b5826a0745828d176b9d8184f8a07faecbdd9a7
Ir <1319138081@qq.com>
2020-01-31 20:19:09 +0800

    custom-thread-pool completed (#8)
    custom-thread-pool completed (#8)

--- src/main/java/com/github/hcsp/multithread/MyThreadPools.java
+++ src/main/java/com/github/hcsp/multithread/MyThreadPools.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.multithread;
 
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 
 public class MyThreadPools {
     // 创建一个线程池，满足以下要求：
@@ -8,6 +8,36 @@ public class MyThreadPools {
     // 任务等待队列大小为20，如果超过20继续往该线程池中提交任务，这些任务会被悄悄丢弃
     // 线程的名字为"MyThread"
     public static ExecutorService myThreadPool() {
-        return null;
+        return new ThreadPoolExecutor(
+                10,
+                10,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(20),
+                new MyThreadFactory(),
+                new ThreadPoolExecutor.DiscardPolicy()
+        );
+    }
+
+    static class MyThreadFactory implements ThreadFactory {
+        private final ThreadGroup group;
+
+        MyThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() :
+                    Thread.currentThread().getThreadGroup();
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r, "MyThread", 0);
+            if (t.isDaemon()) {
+                t.setDaemon(false);
+            }
+            if (t.getPriority() != Thread.NORM_PRIORITY) {
+                t.setPriority(Thread.NORM_PRIORITY);
+            }
+            return t;
+        }
     }
 }
