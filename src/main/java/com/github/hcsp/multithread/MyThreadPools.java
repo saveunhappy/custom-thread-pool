77cf4312ab401d8b83715eb08f5700ea824e94c2
王杰 <wangjie_fourth@163.com>
2020-03-24 08:30:22 +0800

    [add]：自定义一个线程池 (#14)
    [add]：自定义一个线程池 (#14)

--- src/main/java/com/github/hcsp/multithread/MyThreadPools.java
+++ src/main/java/com/github/hcsp/multithread/MyThreadPools.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.multithread;
 
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 
 public class MyThreadPools {
     // 创建一个线程池，满足以下要求：
@@ -8,6 +8,15 @@ public class MyThreadPools {
     // 任务等待队列大小为20，如果超过20继续往该线程池中提交任务，这些任务会被悄悄丢弃
     // 线程的名字为"MyThread"
     public static ExecutorService myThreadPool() {
-        return null;
+        return new ThreadPoolExecutor(10, 10, 60, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(20),
+                new HasNameThreadFactory(), new ThreadPoolExecutor.DiscardPolicy());
+    }
+
+    private static class HasNameThreadFactory implements ThreadFactory {
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "MyThread");
+        }
     }
 }
