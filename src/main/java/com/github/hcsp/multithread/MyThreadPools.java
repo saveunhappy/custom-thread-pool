127f56596152db353a579e8c90c7d1048bd88948
async <lingyidx@gmail.com>
2021-02-07 17:46:26 +0800

    feat: 定义自定义线程池 (#24)
    feat: 定义自定义线程池 (#24)

--- src/main/java/com/github/hcsp/multithread/MyThreadPools.java
+++ src/main/java/com/github/hcsp/multithread/MyThreadPools.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.multithread;
 
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 
 public class MyThreadPools {
     // 创建一个线程池，满足以下要求：
@@ -8,6 +8,22 @@ public class MyThreadPools {
     // 任务等待队列大小为20，如果超过20继续往该线程池中提交任务，这些任务会被悄悄丢弃
     // 线程的名字为"MyThread"
     public static ExecutorService myThreadPool() {
-        return null;
+        return new ThreadPoolExecutor(
+                10,
+                10,
+                1,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(20),
+                r -> new Thread(null, r, "MyThread"),
+                // 拒绝策略
+                (r, executor) -> {
+                    if (!executor.isShutdown()) {
+                        if (r instanceof FutureTask) {
+                            // 未能正确处理的任务需要及时取消掉，避免任务阻塞处理
+                            ((FutureTask<?>) r).cancel(true);
+                        }
+                    }
+                }
+        );
     }
 }
