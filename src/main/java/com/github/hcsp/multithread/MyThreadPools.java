5eddc03863468f2a251cf566b9e464446b30d3a4
BradyWZH <790406047@qq.com>
2021-02-25 21:52:40 +0800

    自定义线程池 (#25)
    自定义线程池 (#25)

--- src/main/java/com/github/hcsp/multithread/MyThreadPools.java
+++ src/main/java/com/github/hcsp/multithread/MyThreadPools.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class MyThreadPools {
     // 创建一个线程池，满足以下要求：
@@ -8,6 +12,27 @@ public class MyThreadPools {
     // 任务等待队列大小为20，如果超过20继续往该线程池中提交任务，这些任务会被悄悄丢弃
     // 线程的名字为"MyThread"
     public static ExecutorService myThreadPool() {
-        return null;
+        BlockingQueue<Runnable> queue = new MyBlockingQueue<>(20);
+
+        ThreadPoolExecutor executorService = new ThreadPoolExecutor(10, 10, 1, TimeUnit.MINUTES, queue, r -> {
+            Thread thread = new Thread(r);
+            thread.setName("MyThread");
+            return thread;
+        });
+
+        return executorService;
     }
+
+    static class MyBlockingQueue<E> extends ArrayBlockingQueue<E> {
+        MyBlockingQueue(int capacity) {
+            super(capacity);
+        }
+
+        @Override
+        public boolean offer(E e) {
+            super.offer(e);
+            return true;
+        }
+    }
+
 }
